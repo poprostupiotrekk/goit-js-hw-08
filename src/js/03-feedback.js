6ac448254668776606f241ebbf7cc2b10ad007a1
Piotr Kulesza <127444918+poprostupiotrekk@users.noreply.github.com>
2023-08-26 12:54:11 +0200

    poprawki

---
+++ src/js/03-feedback.js
@@ -0,0 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const fbackForm = document.querySelector('feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+fbackForm.addEventListener('input', throttle(onDataInput, 500));
+fbackForm.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+const { email, message } = fbackForm.elements;
+pageReload();
+
+const pageReload = () => {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+};
+
+const onDataInput = e => {
+  const { email, message } = e.currentTarget.elements;
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  const { email, message } = e.currentTarget.elements;
+  console.log(`email: ${email.value}, message: ${message.value}`);
+
+  if (email.value === '' || message.value === '') {
+    return alert(`Please fill in all required fields.`);
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+  dataForm = {};
+};
