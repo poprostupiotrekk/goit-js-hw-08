306cb40a89bb8c9d7f27c7cbd141b1f7f5a22baa
Piotr Kulesza <127444918+poprostupiotrekk@users.noreply.github.com>
2023-09-03 16:00:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,36 @@
 import throttle from 'lodash.throttle';
 
-const fbackForm = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+
+const fbackForm = document.querySelector('.feedback-form');
+const { email, message } = fbackForm.elements;
 
 let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-const { email, message } = fbackForm.elements;
-
-const pageReload = () => {
-  if (dataForm) {
-    email.value = dataForm.email || '';
-    message.value = dataForm.message || '';
-  }
-};
-
 const onDataInput = e => {
-  const { email, message } = e.currentTarget.elements;
   dataForm = { email: email.value, message: message.value };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 };
 
 const onFormSubmit = e => {
   e.preventDefault();
-  const { email, message } = e.currentTarget.elements;
-  console.log(`email: ${email.value}, message: ${message.value}`);
-
+  
   if (email.value === '' || message.value === '') {
     return alert(`Please fill in all required fields.`);
   }
+  
+  console.log({ email: email.value, message: message.value });
 
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
   dataForm = {};
+};
+
+const pageReload = () => {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
 };
 
 fbackForm.addEventListener('input', throttle(onDataInput, 500));
